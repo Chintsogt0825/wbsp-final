92dcc1389db4f41181c15885a79f3fd1bc897a36
113021194 <77044659+Chintsogt0825@users.noreply.github.com>
2025-06-09 21:21:08 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -35,9 +35,6 @@
                         <li class="nav-item">
                             <a class="nav-link" href="<?php echo SITE_URL; ?>/login.php">Login</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo SITE_URL; ?>/register.php">Register</a>
-                        </li>
                     <?php endif; ?>
                 </ul>
             </div>
